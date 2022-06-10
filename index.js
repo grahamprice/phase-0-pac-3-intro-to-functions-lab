0c3efc4fde5eb37b6e30ca74755185f4aa263a7f
Graham <thegrahamprice@icloud.com>
2022-06-10 11:39:29 -0600

    commit tree 9a0bbab51c013e98066c475ecf18dcf79fe0e1b0

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log('HELLO')
+    return string.toUpperCase()
+    ;
+}
+function logWhisper(string) {
+    console.log('hello')
+    return string.toLowerCase();
+}
+function sayHiToHeadphonedRoommate(string) {
+    console.log("Let's have dinner together!")
+    return string.console.log("I would love to!")
+}
